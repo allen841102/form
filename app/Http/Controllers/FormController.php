8dd5922279120ccbb346f075effc1563067fc9cb
Kent Ma <kent.ma@33tech.co>
2020-03-29 20:52:07 +0800

    add form list page

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -10,38 +10,42 @@ class FormController extends Controller
 
   public function index()
   {
-  	//TODO show list of respondent
+      $forms = Form::all();
 
+      return view('list', ['list'=>$forms]);
   }
-  
+
   public function create()
   {
-    return view('form'); 
+    return view('form');
   }
 
   public function store(Request $request)
   {
-  	$fields = ['firstname'=> $request->input('fname'), 
+  	$fields = ['firstname'=> $request->input('fname'),
   			   'lastname' =>$request->input('lname'),
   			   'email' => $request->input('email')];
 
   	$form = Form::create($fields);
 
-	
+
 	return redirect()->route('form.show', ['id'=>$form->id]);
-  	
-  } 
+
+  }
 
   public function show($id)
   {
 
   	$form = Form::find($id);
-  	
-  	return view('show', ['firstname' => $form->firstname , 
-  						  'lastname' => $form->lastname , 
+
+  	return view('show', ['firstname' => $form->firstname ,
+  						  'lastname' => $form->lastname ,
   						  'email' =>$form->email ]);
 
   }
+
+
+
 }
 
 
