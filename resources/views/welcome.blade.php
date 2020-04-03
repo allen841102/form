ea7224803b492c3c4fa334e48aa78ecb7a09b88b
Kent Ma <kent.ma@33tech.co>
2020-04-03 18:40:15 +0800

    update home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,10 @@
 
 <div class="container">
 
-    <div class="jumbotron">
+    <div class="jumbotron bg-white">
         <h1 class="display-4"> 歡迎來到 {{ config('app.name') }}!</h1>
-        <p class="lead"> 這是一個線上問券研究的 Side Project，後端用 PHP 框架 Laravel，前端用 Vue 框架 Element UI </p>
         <hr class="my-4">
-        <p> 登入以開始維護你的線上問券吧 !</p>
+        <p> 開始維護你的線上問券吧 !</p>
         <a class="btn btn-primary btn-lg btn-block" href="{{ route('login') }}" role="button">{{__('Login')}}</a>
     </div>
 </div>
