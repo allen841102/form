54c69a0c2fb2925a864e2e284df6802ab2e6516e
Kent Ma <kent.ma@33tech.co>
2020-04-18 20:44:26 +0800

    update index page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,32 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.home');
+        $surveyList = [
+                     [
+                         'sequence'=> 1,
+                         'title'=> '飲食習慣研究',
+                         'created_at'=> '2020-01-02 12:39:49',
+                         'updated_at'=> '2020-04-03 18:00:22',
+                         'question_count'=> 5,
+                         'status'=> '開啟',
+                         'response_count'=> 33,
+                         'response_time'=> '2020-03-31 14:23:12',
+                         'view_link'=> '/admin/survey/1',
+                         'edit_link'=> '/admin/survey/edit/1'
+                     ],
+                     [
+                         'sequence'=> 2,
+                         'title'=> '大學生打工調查',
+                         'created_at'=> '2020-02-22 12:39:49',
+                         'updated_at'=> '2020-03-01 18:00:22',
+                         'question_count'=> 15,
+                         'status'=> '關閉',
+                         'response_count'=> 123,
+                         'response_time'=> '2020-02-28 14:23:12',
+                         'view_link'=> '/admin/survey/1',
+                         'edit_link'=> '/admin/survey/edit/1'
+                     ]
+                 ];
+        return view('admin.home', ['list'=>json_encode($surveyList)]);
     }
 }
--- resources/admin-js/components/SurveyTable.vue
+++ resources/admin-js/components/SurveyTable.vue
@@ -65,40 +65,15 @@
 </template>
 <script>
     export default {
-        // props: {
-        //     surveyList: {
-        //         type: Array,
-        //         required: true,
-        //     }
-        // },
+        props: {
+            surveyList: {
+                type: String,
+                required: true,
+            }
+        },
         data() {
             return {
-                tableData: [
-                    {
-                        'sequence': 1,
-                        'title': '飲食習慣研究',
-                        'created_at': '2020-01-02 12:39:49',
-                        'updated_at': '2020-04-03 18:00:22',
-                        'question_count': 5,
-                        'status': '開啟',
-                        'response_count': 33,
-                        'response_time': '2020-03-31 14:23:12',
-                        'view_link': '/admin/survey/1',
-                        'edit_link': '/admin/survey/edit/1'
-                    },
-                    {
-                        'sequence': 2,
-                        'title': '大學生打工調查',
-                        'created_at': '2020-02-22 12:39:49',
-                        'updated_at': '2020-03-01 18:00:22',
-                        'question_count': 15,
-                        'status': '關閉',
-                        'response_count': 123,
-                        'response_time': '2020-02-28 14:23:12',
-                        'view_link': '/admin/survey/1',
-                        'edit_link': '/admin/survey/edit/1'
-                    }
-                ],
+                tableData: JSON.parse(this.surveyList),
             }
         },
         methods: {
