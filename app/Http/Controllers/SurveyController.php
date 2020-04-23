6c489d26c09c739543b7ca05c15cbdd150c5b914
Kent Ma <kent.ma@33tech.co>
2020-04-24 01:08:09 +0800

    update SurveyController

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Answer;
-use App\Content;
 use App\Master;
-use App\Type;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class SurveyController extends Controller
@@ -34,19 +31,21 @@ class SurveyController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         //Validation
-        $masterFields = ['name'=> $request->input('name'),
-                         'start_text'=>$request->input('start_text'),
-                         'end_text'=>$request->input('end_text'),
-                         'user_id' => Auth::user()->id,
-                         'status'=>Master::STATUS_ACTIVE];
+        $masterFields = [
+            'name'       => $request->input('name'),
+            'start_text' => $request->input('start_text'),
+            'end_text'   => $request->input('end_text'),
+            'user_id'    => Auth::user()->id,
+            'status'     => Master::STATUS_ACTIVE
+        ];
         $questions = $request->input('questions');
-
         $seq = 1;
         $questionFields = [];
         foreach ($questions as $question) {
@@ -54,22 +53,20 @@ class SurveyController extends Controller
             $answers = [];
             foreach ($question['answers'] as $value) {
                 $answers[] = [
-                    'text'=>$value,
-                    'seq'=>$ansSeq++
+                    'text' => $value,
+                    'seq'  => $ansSeq++
                     //'content_id'
                 ];
             }
-
             $questionFields[] = [
-                'seq'=>$seq++,
-                'title'=>$question['title'],
-                'required' => (int) $question['required'],
-                'type_id' =>$question['type'],
-                'answers' => $answers
+                'seq'      => $seq++,
+                'title'    => $question['title'],
+                'required' => (int)$question['required'],
+                'type_id'  => $question['type'],
+                'answers'  => $answers
                 //'master_id'
             ];
         }
-
         $master = Master::create($masterFields);
         foreach ($questionFields as $fields) {
             $attrs = $fields;
@@ -81,13 +78,15 @@ class SurveyController extends Controller
                 }
             }
         }
-        return response()->json(['url'=> route('dashboard')]);
+
+        return response()->json(['url' => route('dashboard')]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -98,7 +97,8 @@ class SurveyController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -107,56 +107,90 @@ class SurveyController extends Controller
         foreach ($survey->contents as $content) {
             $content['type'] = $content->type_id;
             unset($content->type_id);
-            foreach ($content->answers as $key=>$answerModel) {
+            foreach ($content->answers as $key => $answerModel) {
                 $content->answers[$key] = $answerModel->text;
             }
         }
-        return view('admin.edit', ['survey'=>json_encode($survey)]);
+
+        return view('admin.edit', ['survey' => json_encode($survey)]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int                      $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        $master = Master::where('user_id' , Auth::id())
-                         ->where('id' , $id)
-                         ->update(['name' => $request->input('name'),
-                                   'start_text' => $request->input('start_text'),
-                                    'end_text' => $request->input('end_text')]);
+        $master = Master::where('user_id', Auth::id())
+                        ->where('id', $id)
+                        ->first();
+        //update master
+        $master->update([
+                            'name'       => $request->input('name', '沒有主題'),
+                            'start_text' => $request->input('start_text', '沒有開頭'),
+                            'end_text'   => $request->input('end_text', '沒有結尾')
+                        ]);
 
+        //delete master's relations
+        foreach ($master->contents as $content) {
+            foreach ($content->answers as $answer) {
+                $answer->delete();
+            }
+            $content->delete();
+        }
+
+        //create new relations
         $questions = $request->input('questions');
-
-        foreach ($questions as $question)
-        {
-            $content = $master->contents()->updateOrCreate(['id' => $question['id']],
-                                                           ['title' => $question['title'],
-                                                            'required' => (int) $question['required'],
-                                                            'type_id' => $question['type']
-                                                            ]);
-
-            foreach (question['answers'] as $value)
-            {
-                $content->answers()->updateOrCreate(['id' => $value['id']],
-                                                    ['text' => $value,]);
+        $seq = 1;
+        foreach ($questions as $question) {
+            $content = $master->contents()->create(
+                [
+                    'seq'      => $seq++,
+                    'title'    => $question['title'],
+                    'required' => (int)$question['required'],
+                    'type_id'  => $question['type']
+                ]
+            );
+            $ansSeq = 1;
+            foreach ($question['answers'] as $value) {
+                if (!empty($value)) {
+                    $content->answers()->create(
+                        [
+                            'seq'  => $ansSeq++,
+                            'text' => $value,
+                        ]
+                    );
+                }
             }
         }
 
-
+        return response()->json(['url' => route('dashboard')]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //TODO handle request, deleting the whole survey data with $id
+        $master = Master::where('user_id', Auth::id())
+                        ->where('id', $id)
+                        ->first();
+        //delete master's relations
+        foreach ($master->contents as $content) {
+            foreach ($content->answers as $answer) {
+                $answer->delete();
+            }
+            $content->delete();
+        }
+        $master->delete();
+        return response()->json();
     }
 }
