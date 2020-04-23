2674e6517378fc158d20f99aad3dc265178ddd31
Kent Ma <kent.ma@33tech.co>
2020-04-24 01:35:43 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,33 +9,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     $name = request('name');
 
     return view('welcome', [
-    'name' => $name]);
+        'name' => $name
+    ]);
 });
-
 Route::get('form/{id}', 'FormController@show')->name('form.show');
-
 Auth::routes();
-
 //GET 登入後首頁
 Route::get('admin/dashboard', 'HomeController@index')->name('dashboard');
 
-//GET 建立 Survey 頁面
-Route::get('admin/survey/create', 'SurveyController@create')
-     ->name('survey.create');
-
-//POST 建立 Survey 資料
-Route::post('admin/survey/create', 'SurveyController@store');
-
-//GET 顯示 Survey details
-Route::get('admin/survey/{id}', 'SurveyController@show');
-
-//GET 顯示 Survey 編輯資料
-Route::get('admin/survey/edit/{id}', 'SurveyController@edit');
-
-//PUT 更新 Survey 資料
-Route::put('admin/survey/{id}', 'SurveyController@update');
+Route::prefix('admin/survey')
+     ->middleware('auth')
+     ->group(function () {
+         //GET 建立 Survey 頁面
+         Route::get('create', 'SurveyController@create')
+              ->name('survey.create');
+         //POST 建立 Survey 資料
+         Route::post('create', 'SurveyController@store');
+         //GET 顯示 Survey details
+         Route::get('{id}', 'SurveyController@show');
+         //GET 顯示 Survey 編輯資料
+         Route::get('edit/{id}', 'SurveyController@edit');
+         //PUT 更新 Survey 資料
+         Route::put('{id}', 'SurveyController@update');
+     });
