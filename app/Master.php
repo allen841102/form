d336202f9d4596a435c8c07cf581e02032926826
Allen Hung <n0975763692@gmail.com>
2020-04-23 16:41:09 +0800

    0423 v2

--- app/Master.php
+++ app/Master.php
@@ -10,6 +10,10 @@ class Master extends Model
     {
         return $this->hasMany(Content::class);
     }
+    public function user()
+    {
+        return $this->belongsToMany('App\User');
+    }
     protected $table = 'Master';
     public $timestamps = false;
     protected $guarded = [];
