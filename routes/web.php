2935802bc81a53da1a51bfccc07cc92d09261bdd
Kent Ma <kent.ma@33tech.co>
2020-04-23 19:09:46 +0800

    add comment

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@
 */
 
 Route::get('/', function () {
-
     $name = request('name');
 
     return view('welcome', [
@@ -23,14 +22,20 @@ Route::get('form/{id}', 'FormController@show')->name('form.show');
 
 Auth::routes();
 
+//GET 登入後首頁
 Route::get('admin/dashboard', 'HomeController@index')->name('dashboard');
 
+//GET 建立 Survey 頁面
 Route::get('admin/survey/create', 'SurveyController@create')->name('survey.create');
 
+//POST 建立 Survey 資料
 Route::post('admin/survey/create', 'SurveyController@store');
 
+//GET 顯示 Survey details
 Route::get('admin/survey/{id}', 'SurveyController@show');
 
+//GET 顯示 Survey 編輯資料
 Route::get('admin/survey/edit/{id}', 'SurveyController@edit');
 
+//PUT 更新 Survey 資料
 Route::put('admin/survey/{id}', 'SurveyController@update');
