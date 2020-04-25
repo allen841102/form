57d1a200ac7cd60f9581249931a58f7b1f706a9d
Kent Ma <kent.ma@33tech.co>
2020-04-26 00:18:08 +0800

    update logic updaing survey

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -7,6 +7,7 @@ use App\Content;
 use App\Master;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class SurveyController extends Controller
 {
@@ -105,7 +106,13 @@ class SurveyController extends Controller
      */
     public function edit($id)
     {
-        $survey = Master::find($id)->load('contents.answers');
+        $survey = Master::with(['contents'=> function ($query) {
+            $query->orderBy('seq')
+                  ->with(['answers'=> function ($query) {
+                      $query->orderBy('seq');
+                  }]);
+        }])->find($id);
+
         foreach ($survey->contents as $content) {
             $content['type'] = $content->type_id;
             unset($content->type_id);
@@ -127,47 +134,84 @@ class SurveyController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $master = Master::where('user_id', Auth::id())
-                        ->where('id', $id)
-                        ->with('contents')
-                        ->first();
-        //update master
-        $master->update([
-                            'name'       => $request->input('name', '沒有主題'),
-                            'start_text' => $request->input('start_text', '沒有開頭'),
-                            'end_text'   => $request->input('end_text', '沒有結尾')
-                        ]);
-        //update or create relations
-        $questions = $request->input('questions');
-        $contentSeq = 1;
-        foreach ($questions as $question) {
-            $attrs = [
-                'seq'      => $contentSeq++,
-                'title'    => $question['title'],
-                'required' => (int)$question['required'],
-                'type_id'  => $question['type'],
-            ];
-            if (is_null($question['id'])) {
-                $content = $master->contents()->create($attrs);
-            } else {
-                $content = Content::where('id', $question['id'])->first();
-                $content->update($attrs);
-            }
-            $ansSeq = 1;
-            foreach ($question['answers'] as $newAnswer) {
-                $ansAttrs = [
-                        'seq'  => $ansSeq++,
-                        'text' => $newAnswer['text'],
-                    ];
-                if (empty($newAnswer['id'])) {
-                    $content->answers()->create($ansAttrs);
-                } else {
-                    Answer::where('id', $newAnswer['id'])->update($ansAttrs);
+        try {
+            DB::beginTransaction();
+            $master = Master::where('user_id', Auth::id())
+                            ->where('id', $id)
+                            ->with('contents')
+                            ->first();
+            //update master
+            $master->update([
+                                'name'       => $request->input('name', '沒有主題'),
+                                'start_text' => $request->input('start_text', '沒有開頭'),
+                                'end_text'   => $request->input('end_text', '沒有結尾')
+                            ]);
+
+            $questions = $request->input('questions');
+
+            // 刪除題目
+            foreach ($master->contents as $content) {
+                $found = false;
+                foreach ($questions as $question) {
+                    if ($content->id == $question['id']) {
+                        $found = true;
+                        break;
+                    }
+                }
+                if ($found==false) {
+                    $content->answers()->delete();
+                    $content->delete();
                 }
             }
-        }
 
-        return response()->json(['url' => route('dashboard')]);
+            //更新 或 建立 題目與答案
+            $contentSeq = 1;
+            foreach ($questions as $question) {
+                $attrs = [
+                    'seq'      => $contentSeq++,
+                    'title'    => $question['title'],
+                    'required' => (int)$question['required'],
+                    'type_id'  => $question['type'],
+                ];
+                if (is_null($question['id'])) {
+                    $content = $master->contents()->create($attrs);
+                } else {
+                    $content = Content::where('id', $question['id'])->first();
+                    $content->update($attrs);
+                }
+
+                //刪除答案
+                foreach ($content->answers as $answer) {
+                    $found = false;
+                    foreach ($question['answers'] as $userAnswer) {
+                        if ($answer->id == $userAnswer['id']) {
+                            $found = true;
+                            break;
+                        }
+                    }
+                    if ($found==false) {
+                        $answer->delete();
+                    }
+                }
+
+                $ansSeq = 1;
+                foreach ($question['answers'] as $userAnswer) {
+                    $ansAttrs = [
+                        'seq'  => $ansSeq++,
+                        'text' => $userAnswer['text'],
+                    ];
+                    if (empty($userAnswer['id'])) {
+                        $content->answers()->create($ansAttrs);
+                    } else {
+                        Answer::where('id', $userAnswer['id'])->update($ansAttrs);
+                    }
+                }
+            }
+            DB::commit();
+            return response()->json(['url' => route('dashboard')]);
+        } catch (\Exception $e) {
+            DB::rollBack();
+        }
     }
 
     /**
