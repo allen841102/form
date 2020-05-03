597114827ad9ec5198acd582ac6fd2d358656b9d
Kent Ma <kent.ma@33tech.co>
2020-05-03 23:15:16 +0800

    update share vue

--- resources/admin-js/components/SurveyDetails/SurveyShare.vue
+++ resources/admin-js/components/SurveyDetails/SurveyShare.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
-        分享問券
-        <span>
-        {{ url }}
-         </span>
-        <img :src="qrcode">
+        <div class="share-row">
+            分享網址: <a class="share-url" :href="url">{{ url }}</a>
+         </div>
+        <div class="share-row">
+        <span>QR Code: </span>
+        <img :src="qrcode" class="share-qrcode">
+        </div>
     </div>
 </template>
 
@@ -18,3 +20,18 @@
         }
     }
 </script>
+<style>
+    .share-qrcode {
+        height: 200px;
+        width: 200px;
+        vertical-align: text-top;
+    }
+    .share-url {
+        color: #409EFF;
+        font-size: 1rem;
+    }
+    .share-row {
+        padding: 5px;
+        margin: auto;
+    }
+</style>
